05cc848b7ae2d0518b900a09c0251abdfc095adf
harshiv7 <harshiv778@gmail.com>
2019-01-18 19:55:53 +0530

    bit of styling

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ $(document).ready(function(){
     $("#signup_btn").click(function(){
         $("#main").animate({left:"22.5%"},400);
         $("#main").animate({left:"30%"},500);
+
+        $("#signup").animate({left:"17%"},400);
+        $("#signup").animate({left:"30%"},500);
+        $("#signup").css("visibility","visible");
+
         $("#loginform").css("visibility","hidden");
         $("#loginform").animate({left:"25%"},400);
     });
@@ -10,9 +15,13 @@ $(document).ready(function(){
     $("#login_btn").click(function(){
         $("#main").animate({left:"77.5%"},400);
         $("#main").animate({left:"70%"},500);
+       
         $("#loginform").animate({left:"83.5%"},400);
         $("#loginform").animate({left:"70%"},500);
         $("#loginform").css("visibility","visible");
+
+        $("#signup").css("visibility","hidden");
+        $("#signup").animate({left:"75%"},400);
     });
 });
 
